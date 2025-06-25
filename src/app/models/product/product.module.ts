ffc2d8c384d7b1e118673b962b809b8f87ae98da
ervinjahic65 <ervinjahic65@gmail.com>
2025-06-25 13:16:56 +0200

    set up routing

--- src/app/models/product/product.module.ts
+++ src/app/models/product/product.module.ts
@@ -10,3 +10,32 @@ import { CommonModule } from '@angular/common';
   ]
 })
 export class ProductModule { }
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: {
+    width: number;
+    height: number;
+    depth: number;
+  };
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: any[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: any;
+  images: string[];
+  thumbnail: string;
+}
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Product } from '../models/product/product.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<any>('assets/products.json').pipe(
+      map(response => response.products)
+    );
+  }
 }
