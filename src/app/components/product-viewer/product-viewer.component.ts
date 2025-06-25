e8774cfc2314bd8f43ddc3b2c3229a2b1d2ce0fe
ervinjahic65 <ervinjahic65@gmail.com>
2025-06-25 15:57:04 +0200

    add logic and display data in two views

--- src/app/components/product-viewer/product-viewer.component.ts
+++ src/app/components/product-viewer/product-viewer.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product/product.module';
+
+// PrimeNG modules
+import { TabViewModule } from 'primeng/tabview';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-product-viewer',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    TabViewModule,
+    TableModule,
+    InputTextModule,
+    CardModule
+  ],
   templateUrl: './product-viewer.component.html',
-  styleUrl: './product-viewer.component.scss'
+  styleUrls: ['./product-viewer.component.scss']
 })
-export class ProductViewerComponent {
+export class ProductViewerComponent implements OnInit {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  filterText = '';
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = products;
+    });
+  }
+
+  onFilterChange() {
+    const query = this.filterText.toLowerCase();
+    this.filteredProducts = this.products.filter(p =>
+      p.title.toLowerCase().includes(query) ||
+      p.description.toLowerCase().includes(query)
+    );
+  }
 }
